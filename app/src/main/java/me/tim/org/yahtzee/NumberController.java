402459f4f94680a332bc4a8cf4a76be32e4a9b8d
binq1000 <t.daniels@student.fontys.nl>
2016-05-03 21:12:09 +0200

    Changed score completely.

--- app/src/main/java/me/tim/org/yahtzee/NumberController.java
+++ app/src/main/java/me/tim/org/yahtzee/NumberController.java
@@ -1,6 +1,7 @@
 package me.tim.org.yahtzee;
 
 import android.app.Activity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -26,9 +27,6 @@ public class NumberController {
 
     private boolean firstThrow = true;
     private int throwAmount;
-    private ListView listView;
-    private List<ScoreItem> scoreItems;
-    private CustomList adapter;
 
     public NumberController(Activity activity) {
 
@@ -42,84 +40,116 @@ public class NumberController {
         setListeners();
         toggleEnableToggleButton(false);
         throwAmount = 0;
-
-        setListView();
     }
 
+    private void processScore(String item) {
 
-    private void setListView() {
-        listView = (ListView) activity.findViewById(R.id.list);
-        scoreItems = getScoreKeys();
-        adapter = new CustomList(activity, scoreItems);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //TODO on item clicked
-                if (!firstThrow) {
-                    processScores(scoreItems.get(+position));
-                }
-            }
-        });
-    }
-
-    private void processScores(ScoreItem scoreItem) {
-        if (scoreItem.getScore() != -1) {
+        if (firstThrow) {
             return;
         }
 
         List<Integer> numbers = new ArrayList<>();
+        int totalSum = 0;
+
         ListIterator<ToggleButton> it = toggleButtons.listIterator();
         while (it.hasNext()) {
             ToggleButton toggleButton = it.next();
             numbers.add(Integer.valueOf(toggleButton.getText().toString()));
+            totalSum += Integer.valueOf(toggleButton.getText().toString());
         }
 
         int value = 0;
 
-        switch (scoreItem.getName()) {
+        TextView textView = null;
+
+        //region Switch
+        switch (item) {
             case "Ones":
-                value = Collections.frequency(numbers, 1);
-                scoreItem.setScore(value);
+                if (!setSingleScore((TextView) activity.findViewById(R.id.textValueOne), Collections.frequency(numbers, 1))) {
+                    return;
+                }
                 break;
             case "Twos":
-                value = Collections.frequency(numbers, 2) * 2;
-                scoreItem.setScore(value);
+                if (!setSingleScore((TextView) activity.findViewById(R.id.textValueTwo), Collections.frequency(numbers, 2) * 2)) {
+                    return;
+                }
                 break;
             case "Threes":
-                value = Collections.frequency(numbers, 3) * 3;
-                scoreItem.setScore(value);
+                if (!setSingleScore((TextView) activity.findViewById(R.id.textValueThree), Collections.frequency(numbers, 3) * 3)) {
+                    return;
+                }
                 break;
             case "Fours":
-                value = Collections.frequency(numbers, 4) * 4;
-                scoreItem.setScore(value);
+                if (!setSingleScore((TextView) activity.findViewById(R.id.textValueFour), Collections.frequency(numbers, 4) * 4)) {
+                    return;
+                }
                 break;
             case "Fives":
-                value = Collections.frequency(numbers, 5) * 5;
-                scoreItem.setScore(value);
+                if (!setSingleScore((TextView) activity.findViewById(R.id.textValueFive), Collections.frequency(numbers, 5) * 5)) {
+                    return;
+                }
                 break;
             case "Sixes":
-                value = Collections.frequency(numbers, 6) * 6;
-                scoreItem.setScore(value);
+                if (!setSingleScore((TextView) activity.findViewById(R.id.textValueSix), Collections.frequency(numbers, 6) * 6)) {
+                    return;
+                }
+                break;
+            case "ThreeOfAKind":
+                //TODO
+                if (checkAllMultipleOccurrences(numbers, 3)) {
+                    if (!setSingleScore((TextView) activity.findViewById(R.id.textValueThreeOfAKind), totalSum)) {
+                        return;
+                    }
+                }
+                break;
+            case "FourOfAKind":
+                //TODO
+                break;
+            case "SmlStraigth":
+                //TODO
+                break;
+            case "LngStraigth":
+                //TODO
+                break;
+            case "FullHouse":
+                //TODO
+                break;
+            case "Yahtzee":
+                //TODO
+                break;
+            case "Chance":
+                //TODO
                 break;
             default:
                 throw new IllegalArgumentException("Input not found");
         }
+        //endregion
 
-        TextView textView = (TextView) activity.findViewById(R.id.totalTextValue);
-        textView.setText(String.valueOf(Integer.valueOf(textView.getText().toString()) + value));
-        adapter.notifyDataSetChanged();
+        setTotalValue();
         resetNumbers();
 
-        int shouldEnd = 0;
-        for (ScoreItem score : scoreItems) {
-            if (score.getScore() == -1) {
-                shouldEnd++;
+        checkEnd();
+    }
+
+    private boolean checkAllMultipleOccurrences(List<Integer> numbers, int amount) {
+        int counter = 0;
+        for (int i=1; i < 7; i++) {
+            if (Collections.frequency(numbers, i) >= amount) {
+                return true;
             }
         }
-        if (shouldEnd == 0) {
+        return false;
+    }
+
+    private void checkEnd() {
+        TextView textViewOne = (TextView) activity.findViewById(R.id.textValueOne);
+        TextView textViewTwo = (TextView) activity.findViewById(R.id.textValueTwo);
+        TextView textViewThree = (TextView) activity.findViewById(R.id.textValueThree);
+        TextView textViewFour = (TextView) activity.findViewById(R.id.textValueFour);
+        TextView textViewFive = (TextView) activity.findViewById(R.id.textValueFive);
+        TextView textViewSix = (TextView) activity.findViewById(R.id.textValueSix);
+
+        if (!textViewOne.getText().toString().matches("") && !textViewTwo.getText().toString().matches("") && !textViewThree.getText().toString().matches("") && !textViewFour.getText().toString().matches("") && !textViewFive.getText().toString().matches("") && !textViewSix.getText().toString().matches("")) {
             Button button = (Button) activity.findViewById(R.id.btnThrow);
             button.setText("Restart");
             button.setOnClickListener(new View.OnClickListener() {
@@ -129,6 +159,61 @@ public class NumberController {
                 }
             });
         }
+    }
+
+    private boolean setSingleScore(TextView textView, int value) {
+        if (!textView.getText().toString().matches("")) {
+            return false;
+        } else {
+            textView.setText(String.valueOf(value));
+            return true;
+        }
+    }
+
+    private void setTotalValue() {
+        TextView textView = (TextView) activity.findViewById(R.id.totalTextValue);
+        int value = getTotalValue();
+
+        if (value >= 63) {
+            textView.setText(String.valueOf(value) + " + 35\nBonus Acquired");
+        } else {
+            textView.setText(String.valueOf(value));
+        }
+    }
+
+    private int getTotalValue() {
+        int value = 0;
+
+        TextView textView = (TextView) activity.findViewById(R.id.textValueOne);
+        if (!textView.getText().toString().matches("")) {
+            value += Integer.valueOf(textView.getText().toString());
+        }
+
+        textView = (TextView) activity.findViewById(R.id.textValueTwo);
+        if (!textView.getText().toString().matches("")) {
+            value += Integer.valueOf(textView.getText().toString());
+        }
+
+        textView = (TextView) activity.findViewById(R.id.textValueThree);
+        if (!textView.getText().toString().matches("")) {
+            value += Integer.valueOf(textView.getText().toString());
+        }
+
+        textView = (TextView) activity.findViewById(R.id.textValueFour);
+        if (!textView.getText().toString().matches("")) {
+            value += Integer.valueOf(textView.getText().toString());
+        }
+
+        textView = (TextView) activity.findViewById(R.id.textValueFive);
+        if (!textView.getText().toString().matches("")) {
+            value += Integer.valueOf(textView.getText().toString());
+        }
+        textView = (TextView) activity.findViewById(R.id.textValueSix);
+        if (!textView.getText().toString().matches("")) {
+            value += Integer.valueOf(textView.getText().toString());
+        }
+
+        return value;
     }
 
     private void resetNumbers() {
@@ -141,19 +226,10 @@ public class NumberController {
         while (it.hasNext()) {
             ToggleButton toggleButton = it.next();
             toggleButton.setText("");
+            toggleButton.setTextOn("");
+            toggleButton.setTextOff("");
+            toggleButton.setChecked(false);
         }
-    }
-
-    private List<ScoreItem> getScoreKeys() {
-        ArrayList<ScoreItem> scores = new ArrayList();
-        scores.add(new ScoreItem("Ones"));
-        scores.add(new ScoreItem("Twos"));
-        scores.add(new ScoreItem("Threes"));
-        scores.add(new ScoreItem("Fours"));
-        scores.add(new ScoreItem("Fives"));
-        scores.add(new ScoreItem("Sixes"));
-
-        return scores;
     }
 
     public void toggleEnableToggleButton(boolean value) {
@@ -217,6 +293,34 @@ public class NumberController {
                 }
             }
         });
+
+        setCardListeners();
+    }
+
+    public void setCardListeners() {
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardOne), "Ones");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardTwo), "Twos");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardThree), "Threes");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardFour), "Fours");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardFive), "Fives");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardSix), "Sixes");
+
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardThreeOfAKind),  "ThreeOfAKind");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardFourOfAKind),  "FourOfAKind");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardSmallStraight),  "SmlStraigth");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardLongStraigth),  "LngStraigth");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardFullHouse),  "FullHouse");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardYahtzee),  "Yahtzee");
+        setSingleCardListener((CardView) activity.findViewById(R.id.cardChance),  "Chance");
+    }
+
+    private void setSingleCardListener(CardView cardView, final String value) {
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processScore(value);
+            }
+        });
     }
 
     public void generateNewNumbers() {
@@ -252,7 +356,6 @@ public class NumberController {
     public void generateNumbers() {
 
         toggleEnableToggleButton(true);
-
 
         Random r = new Random();
         ToggleButton tButton = (ToggleButton) activity.findViewById(R.id.number1);
@@ -311,13 +414,40 @@ public class NumberController {
             }
         });
 
+        emptyScoreStrings();
+    }
+
+    private void emptyScoreStrings() {
         TextView textView = (TextView) activity.findViewById(R.id.totalTextValue);
         textView.setText("0");
 
-        for (ScoreItem scoreItem : scoreItems) {
-            scoreItem.setScore(-1);
-        }
-
-        adapter.notifyDataSetChanged();
+        //region setting Score labels to an empty string
+        textView = (TextView) activity.findViewById(R.id.textValueOne);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueTwo);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueThree);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueFour);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueFive);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueSix);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueThreeOfAKind);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueFourOfAKind);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueFullHouse);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueSmallStraight);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueLongStraigth);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueYahtzee);
+        textView.setText("");
+        textView = (TextView) activity.findViewById(R.id.textValueChance);
+        textView.setText("");
+        //endregion
     }
 }
